1314a069aea5a86e7584ea05e44ee2d5828af701
gttdo <vinces.gerardo@gmail.com>
2020-12-15 15:41:10 -0800

    second commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+
+const Index = () => {
+    return (
+        <Page>
+            <Layout>
+                <EmptyState
+                    heading="Manage your inventory transfers"
+                    action={{content: 'Select Products'}}
+                    image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+                >
+                    <p>Select Products</p>
+                </EmptyState>
+            </Layout>
+        </Page>
+    )
+}
+
+export default Index
